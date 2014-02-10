e79ae1fca8fc00d826153491b077478a903e990f
Sara Delaney <sara.delaney@allocadia.com>
2014-02-10 00:08:41 -0800

    generic configuration setup in js

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,5 +1,37 @@
+function findProgramSettings(program, week, session) {
+	console.log("findProgramSettings: program = " + program + ", week = " + week + ", session = " + session);
+
+	$.getJSON("resources/programs.json", function(json) {
+		console.log(json);
+	});
+}
+
 Pebble.addEventListener("ready",
     function(e) {
-        console.log("Hello world! - Sent from your javascript application.");
+        console.log("JavaScript app is ready and running.");
     }
+);
+
+Pebble.addEventListener("showConfiguration",
+	function(e) {
+		console.log("Showing Configuration");
+		Pebble.openURL('http://sdelaney.github.io/interbbleTimer/index.html');
+	}
+);
+
+Pebble.addEventListener("webviewclosed",
+	function(e) {
+		console.log("Closing configuration window");
+
+		//get the response object
+		if (e.response) {
+			var config = JSON.parse(e.response);
+			console.log("Configuration window returned: " + JSON.stringify(config));
+
+			findProgramSettings(config.program, config.week, config.session);
+		} else {
+			console.log("no response from configuration");
+		}
+
+	}
 );
